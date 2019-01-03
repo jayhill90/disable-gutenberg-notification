c6465537dbd1e6d68a8e37a15702aa7a9090c0f4
Jay Hill <jordan.m.hill@icloud.com>
2019-01-03 17:49:18 -0600

    removed update nag too.

--- disable-gutenberg-notification.php
+++ disable-gutenberg-notification.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Plugin Name:     Disable Gutenberg Notification
+ * Plugin Name:     Disable Update Notification and Try Gutenberg
  * Plugin URI:      https://testinproduction.systems
- * Description:     Removes the Try Gutenberg notification on the dashboard for WP 4.8+
+ * Description:     Removes the Try Gutenberg notification on the dashboard for WP 4.8+ and removes the 5.0 Update Nag.
  * Author:          Jay Hill
  * Author URI:      https://wpdev.life
  * Text Domain:     disable-gutenberg-notification
@@ -11,5 +11,5 @@
  *
  * @package         Disable_Gutenberg_Notification
  */
-
+remove_action( 'admin_notices', 'update_nag', 3 );
 remove_action( 'try_gutenberg_panel', 'wp_try_gutenberg_panel' );
